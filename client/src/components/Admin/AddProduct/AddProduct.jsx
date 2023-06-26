99fd82f85a6c38e4af081a8a96535a2450d9c8eb
Aytacrzaeva <rzayevaaytac188@gmail.com>
2023-06-26 16:43:53 +0400

    inital

--- client/src/components/Admin/AddProduct/AddProduct.jsx
+++ client/src/components/Admin/AddProduct/AddProduct.jsx
@@ -1,135 +1,258 @@
-import React, { useRef } from "react";
-import { Helmet } from "react-helmet";
-import { Formik } from "formik";
-import * as Yup from "yup";
-import axios from "axios";
-import "./AddProduct.scss";
-import { toast, Toaster } from "react-hot-toast";
+import React, { useState, useEffect } from 'react';
+import {useNavigate, useParams} from 'react-router';
+import { Button, Box, TextField, Select, FormControl,
+    InputLabel, MenuItem, TextareaAutosize,  } from '@mui/material';
 
-export default function AddProduct() {
+const AddProduct = () => {
+    const navigate = useNavigate();
+    const[brands, setBrands] = useState();
+    const[categories, setCategories] = useState();
+    
+    const [form, setForm] = useState({
+        name: '',
+        categoryId: '',
+        brandId: '',
+        stock: '',
+        price: '',
+        desc: '',
+        discount: '',
+        images: [],
+    });
+
+    const { id } = useParams();
+
+    useEffect(()=>{
+        if (id) {
+            (async() => {
+                setForm(await getProductsDetail(id));
+            })()
+        }
+        handleGetBrands();
+        handleGetCategories();
+    }, [id])
+
+    const submitHandler = async (data) => {
+        const postData = new FormData()
+        for(let key in data){
+            if (key === 'images') {
+                data[key].forEach(file => {
+                    postData.append('images', file);
+                })
+            } else {
+                postData.append(key, data[key]);
+            }
+        }
+        await addProduct(postData);
+        navigate('/admin/products-list-page')
+    };
+
+    const onUploadImage = (e) => {
+        const files = Array.from(e.target.files);
+        if (files.length) {
+            setForm({
+                ...form,
+                images: files
+            })
+        }
+    }
+
+    async function handleGetBrands(){
+        setBrands(await getBrands());
+    }
+
+    async function handleGetCategories(){
+        setCategories(await getCategories());
+    }
+
+    async function editHandler(){
+        const postData = new FormData()
+        for(let key in form){
+            if (key === 'images') {
+                form[key].forEach(file => {
+                    postData.append('images', file);
+                })
+            } else {
+                postData.append(key, form[key]);
+            }
+        }
+        await editProduct(id, postData)
+        navigate('/admin/products-list-page')
+    }
+
     return (
-        <>
-            <Helmet>
-                <title>Add</title>
-            </Helmet>
-            <div className="formik">
-                <div className="form">
-                    <Formik
-                        initialValues={{
-                            name: "",
-                            images: [],
-                            rating: "",
-                            instock: false,
-                            size: "",
-                            price: "",
-                            productcode: "",
-                        }}
-                        validationSchema={Yup.object({      
-                            name: Yup.string().required("Name is required"),
-                            images: Yup.mixed().required("Images are required"),
-                            rating: Yup.string().required("Rating is required"),
-                            instock: Yup.boolean().required("InStock is required"),
-                            size: Yup.string().required("Size is required"),
-                            price: Yup.number().required("Price is required"),
-                            productcode: Yup.string().required("Product Code is required"),
-                        })}
-                        onSubmit={(values, { resetForm }) => {
-                            console.log(values)
-                            axios
-                                .post("http://localhost:8080/products/", values)
-                                .then((res) => {
-                                    toast.success("Added new Card");
+        <LayoutAdmin>
+            <Box display="flex" alignItems="center" justifyContent="center">
+                <form encType="multipart/form-data">
+                    <TextField
+                        style={{ width: "200px", margin: "5px" }}
+                        type="text"
+                        label="Product name"
+                        variant="outlined"
+                        value={form.name}
+                        onChange={(e) =>setForm({
+                            ...form,
+                            name: e.target.value
+                        })
+                        }
+                        required
+                    />
+                    <br />
+                    <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }} required>
+                        <InputLabel id="demo-simple-select-standard-label">Category</InputLabel>
+                        <Select
+                            value={form.categoryId}
+                            onChange={(e) =>setForm({
+                                ...form,
+                                categoryId: e.target.value
+                            })
+                            }
+                            label="Category"
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            {
+                                categories?.map((item) => {
+                                    return <MenuItem
+                                        key={item._id}
+                                        value={item._id}>
+                                        {item.name}
+                                    </MenuItem>
                                 })
-                                .catch((error) => {
-                                    toast.error("Error adding new Card");
-                                });
-                            resetForm();
-                        }}
-                    >
-                        {({ values, setFieldValue,handleChange, handleSubmit, dirty }) => (
-                            <form onSubmit={handleSubmit}>
-                                <div className="form-row">
-                                    <div className="form-field">
-                                        <label htmlFor="name">Name:</label>
-                                        <input
-                                            type="text"
-                                            placeholder="Name"
-                                            id="name"
-                                            value={values.name}
-                                            onChange={handleChange("name")}
-                                        />
-                                    </div>
-                                    <div className="form-field">
-                                        <label htmlFor="images">Images:</label>
-                                        <input
-                                            type="file"
-                                            placeholder="Images"
-                                            id="images"
-                                            name="images"
-                                            value={values.images}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                    <div className="form-field">
-                                        <label htmlFor="rating">Rating:</label>
-                                        <input
-                                            type="text"
-                                            placeholder="Rating"
-                                            id="rating"
-                                            value={values.rating}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                    <div className="form-field">
-                                        <label htmlFor="instock">InStock:</label>
-                                        <input
-                                            type="checkbox"
-                                            id="instock"
-                                            checked={values.instock}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                    <div className="form-field">
-                                        <label htmlFor="size">Size:</label>
-                                        <input
-                                            type="text"
-                                            placeholder="Size"
-                                            id="size"
-                                            value={values.size}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                    <div className="form-field">
-                                        <label htmlFor="price">Price:</label>
-                                        <input
-                                            type="number"
-                                            placeholder="Price"
-                                            id="price"
-                                            value={values.price}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                    <div className="form-field">
-                                        <label htmlFor="productcode">Product Code:</label>
-                                        <input
-                                            type="text"
-                                            placeholder="Product Code"
-                                            id="productcode"
-                                            value={values.productcode}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                </div>
-
-                                <button type="submit" disabled={!dirty}>
-                                    Add
-                                </button>
-                            </form>
-                        )}
-                    </Formik>
-                </div>
-            </div>
-            <Toaster />
-        </>
-    );
+                            }
+                        </Select>
+                    </FormControl>
+                    <br />
+                    <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }} required>
+                        <InputLabel id="demo-simple-select-standard-label">Brand</InputLabel>
+                        <Select
+                            value={form.brandId}
+                            onChange={(e) =>setForm({
+                                ...form,
+                                brandId: e.target.value
+                            })
+                            }
+                            label="Brand"
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            {
+                                brands?.map((item) => {
+                                    return <MenuItem
+                                        key={item._id}
+                                        value={item._id}>
+                                        {item.name}
+                                    </MenuItem>
+                                })
+                            }
+                        </Select>
+                    </FormControl>
+                    <br />
+                    <TextField
+                        style={{ width: "200px", margin: "5px" }}
+                        placeholder="Stock"
+                        InputProps={{ inputProps: { min: 0 } }}
+                        type="number"
+                        label="Stock"
+                        variant="outlined"
+                        value={form.stock}
+                        onChange={(e) =>setForm({
+                            ...form,
+                            stock: e.target.value
+                        })
+                        }
+                        required
+                    />
+                    <br />
+                    <TextField
+                        style={{ width: "200px", margin: "5px" }}
+                        placeholder="Discount"
+                        InputProps={{ inputProps: { min: 0 } }}
+                        type="number"
+                        label="Discount"
+                        variant="outlined"
+                        value={form.discount}
+                        onChange={(e) =>setForm({
+                            ...form,
+                            discount: e.target.value
+                        })
+                        }
+                        required
+                    />
+                    <br />
+                    <TextField
+                        style={{ width: "200px", margin: "5px" }}
+                        type="number"
+                        InputProps={{ inputProps: { min: 0 } }}
+                        label="Price"
+                        placeholder='Price'
+                        variant="outlined"
+                        value={form.price}
+                        onChange={(e) =>setForm({
+                            ...form,
+                            price: e.target.value
+                        })
+                        }
+                        required
+                    />
+                    <br />
+                    <TextareaAutosize
+                        aria-label="empty textarea"
+                        placeholder="Description"
+                        style={{ width: 210, height: 100 }}
+                        value={form.desc}
+                        onChange={(e) =>setForm({
+                            ...form,
+                            desc: e.target.value
+                        })
+                        }
+                        required
+                    />
+                    <br />
+                    <input
+                        style={{ width: "300px", margin: "5px" }}
+                        type="file"
+                        accept="image/png, image/jpeg"
+                        multiple
+                        variant="outlined"
+                        required
+                        onChange={onUploadImage}
+                    />
+                    <br />
+                    {
+                        form.images.map((img, i) => {
+                            if(typeof img === "string"){
+                                return <img src={`http://localhost:8080/${img}`}
+                                            width='50px' height='50px'
+                                            style={{margin: '13px'}}
+                                />
+                            }
+                            else{
+                                const url = URL.createObjectURL(img);
+                                return <img src={url}
+                                            width='50px' height='50px'
+                                            style={{margin: '13px'}}
+                                />
+                            }
+                        })
+                    }
+                    <br />
+                    <Box width='200px' margin='5px' display='flex' alignItems='center' justifyContent='space-between'>
+                        <Button type='button' disabled={!!id} variant="contained" color="primary"
+                                onClick={() => submitHandler(form)}>
+                            Add
+                        </Button>
+                        <Button type='button' disabled={!id} variant="contained" color="primary"
+                                onClick={() => editHandler()}>
+                            Save
+                        </Button>
+                    </Box>
+                    <br />
+                </form>
+            </Box>
+        </LayoutAdmin>
+    )
 }
+
+export default AddProduct
